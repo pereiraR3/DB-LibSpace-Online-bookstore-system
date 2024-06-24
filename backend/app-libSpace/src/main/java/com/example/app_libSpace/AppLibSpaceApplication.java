cf3cdeb943f8a24207e5cbabcd991acf13cc7436
pereiraR3 <anthony_rodriguespereira@outlook.com>
2024-06-24 13:45:27 -0400

    Add Elaboração das Config. do framework Spring-Boot em 'backend' Elaboração da pasta para receber o framework 'react', no caso 'frontend' Elaboração da pasta para receber todas as informações sobre a modelagem de banco de dados, no caso 'modeling-database'

---
+++ backend/app-libSpace/src/main/java/com/example/app_libSpace/AppLibSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.app_libSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLibSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppLibSpaceApplication.class, args);
+	}
+
+}
---
+++ backend/app-libSpace/src/test/java/com/example/app_libSpace/AppLibSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app_libSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLibSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
