4acffc462c7381a111204e46249a3bfd6bda2015
pereiraR3 <anthony_rodriguespereira@outlook.com>
2024-06-26 14:19:56 -0400

    Change Rep. A partir desse momento irei dividir os Repositórios de acordo com a função de cada um

--- database.sql
+++ database.sql
@@ -117,7 +117,7 @@ CREATE TABLE carrinho (
     id BIGSERIAL PRIMARY KEY, 
     id_user BIGINT NOT NULL,
     data_criacao DATE DEFAULT CURRENT_DATE,
-    status BOOLEAN DEFAULT FALSE,
+    status BOOLEAN DEFAULT TRUE NOT NULL,
     CONSTRAINT fk_user_in_carrinho FOREIGN KEY (id_user) REFERENCES users (id)
 );
 
