00f75e9f60597d348427347f59be213b2f4785eb
pereira3r <anthony_rodriguespereira@outlook.com>
2024-07-11 14:46:33 -0400

    Add Table genero & Add Table livro_possui_genero

--- database.sql
+++ database.sql
@@ -96,6 +96,12 @@ CREATE TABLE categoria (
     nome VARCHAR(60) NOT NULL
 );
 
+-- Create table genero
+CREATE TABLE genero (
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(60) NOT NULL
+);
+
 -- Create table livro_possui_categoria
 CREATE TABLE livro_possui_categoria (
     id_livro BIGINT NOT NULL,
@@ -103,6 +109,15 @@ CREATE TABLE livro_possui_categoria (
     CONSTRAINT uq_idlivro_e_idcategoria UNIQUE (id_livro, id_categoria), 
     CONSTRAINT fk_livro_in_livro_possui_categoria FOREIGN KEY (id_livro) REFERENCES livro (id) ON DELETE CASCADE,
     CONSTRAINT fk_categoria_in_livro_possui_categoria FOREIGN KEY (id_categoria) REFERENCES categoria (id) ON DELETE CASCADE
+);
+
+-- Create table livro_possui_genero
+CREATE TABLE livro_possui_genero (
+    id_livro BIGINT NOT NULL,
+    id_genero BIGINT NOT NULL,
+    CONSTRAINT uq_idlivro_e_idgenero UNIQUE (id_livro, id_genero), 
+    CONSTRAINT fk_livro_in_livro_possui_categoria FOREIGN KEY (id_livro) REFERENCES livro (id) ON DELETE CASCADE,
+    CONSTRAINT fk_categoria_in_livro_possui_categoria FOREIGN KEY (id_genero) REFERENCES genero (id) ON DELETE CASCADE
 );
 
 -- Create table oferta
